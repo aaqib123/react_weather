336f71d869e6e1fb80bb2f085cfb57f1118e2d0a
Aaqib <aaqib.hyd@gmail.com>
2019-01-02 15:41:49 -0400

    weather api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Titles from './components/title';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Titles from "./components/title";
+import Forms from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "45ca5295995ba7c9abe2680e502765e7";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    //test
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if (city && country) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_call.json();
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+      document.getElementById("cityForm").reset();
+    } else {
+      this.setState({
+        error: "Please enter City and Country."
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <p>ssup</p>
-        <Titles/>
+        <Titles />
+        <Forms getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
